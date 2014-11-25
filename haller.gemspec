077df4e4ad5362e92cde0d780d526b16514ad227
kobayakawa <can9119@gmail.com>
2014-11-25 17:12:01 +0200

    First implementation and tests

--- haller.gemspec
+++ haller.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |spec|
   spec.version       = Haller::VERSION
   spec.authors       = ["kobayakawa"]
   spec.email         = ["can9119@gmail.com"]
-  spec.summary       = %q{TODO: Write a short summary. Required.}
-  spec.description   = %q{TODO: Write a longer description. Optional.}
+  spec.summary       = %q{@sercaneraslan'ın Haller.js'inin Ruby portu}
+  spec.description   = %q{}
   spec.homepage      = ""
   spec.license       = "MIT"
 
@@ -18,6 +18,9 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
+  spec.add_dependency 'unicode_utils'
+
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
+  spec.add_development_dependency 'rspec'
 end
--- lib/haller.rb
+++ lib/haller.rb
@@ -1,5 +1,43 @@
-require "haller/version"
+require 'unicode_utils'
 
-module Haller
-  # Your code goes here...
+class String
+  def to_hal(hal)
+    _isim = isim = UnicodeUtils.downcase self, :tr
+
+    isim = isim.split.last
+
+    iyelik   = 'iyelik'
+    i_hali   = 'i'
+    e_hali   = 'e'
+    de_hali  = 'de'
+    den_hali = 'den'
+    i_ekleri = 'ııiiuuüü'
+
+    son_harf = isim[-1,1]
+
+    istisna  = (isim =~ /(^[ei][^ıüoö]*[au]l$)|^alp$/).nil? ? 0 : 2
+
+    son_sesli = isim.scan(/[aıeiouöü]/).last
+
+    if hal == iyelik || hal == i_hali
+      ek = i_ekleri['aıeiouöü'.index(son_sesli) + istisna]
+    else
+      ek = (son_sesli =~ /[aıou]/).nil? ? 'e' : (istisna == 2) ? 'e' : 'a'
+    end
+
+    if son_harf == son_sesli
+      ek = (hal == iyelik) ? 'n' + ek : (hal == i_hali || hal == e_hali) ? 'y' + ek : ek
+    end
+
+    # Harf yumusamalarini kontrol eder
+    if hal == de_hali || hal == den_hali
+      ek = (son_harf =~ /[fstkçşhp]/ ? 't' : 'd') + ek
+    end
+
+    if hal == iyelik || hal == den_hali
+      ek << 'n'
+    end
+
+    "#{_isim.split.map(&:capitalize)*' '}'#{ek}"
+  end
 end
